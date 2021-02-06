13c47f5e82999ddbb4f8e3ce7ea01d5c6606ca95
Olivier Dulcy <olivier.dulcy@gmail.com>
2021-02-07 00:32:38 +0100

    First implementation in PyTorch, but not as fast as expected

--- random_forest_pytorch/random_forest_gemm_numpy.py
+++ random_forest_pytorch/random_forest_gemm_numpy.py
@@ -1,6 +1,4 @@
-import torch
 import numpy as np
-
 from utils import compute_path
 
 class DecisionTreeGemm:
@@ -53,33 +51,20 @@ class DecisionTreeGemm:
 
         for i in range(len(leaf_nodes)):
             self.E[i,leaf_to_class[i]] = 1
-        
-        self.A = torch.from_numpy(self.A)
-        self.B = torch.from_numpy(self.B)
-        self.C = torch.from_numpy(self.C)
-        self.D = torch.from_numpy(self.D)
-        self.E = torch.from_numpy(self.E)
-        
-        if torch.cuda.is_available():
-            self.A = self.A.to('cuda')
-            self.B = self.B.to('cuda')
-            self.C = self.C.to('cuda')
-            self.D = self.D.to('cuda')
-            self.E = self.E.to('cuda')
     
     def _GEMM(self, X):
         """Implement GEMM Strategy"""
-        T = X.matmul(self.A)
-        T = (T < self.B).type(torch.float64)
-        T = T.matmul(self.C)
-        T = (T == self.D).type(torch.float64)
-        T = T.matmul(self.E)
+        T = X.dot(self.A)
+        T = T < self.B
+        T = T.dot(self.C)
+        T = T == self.D
+        T = T.dot(self.E)
         return T
 
     def predict(self, X):
         """Return class (integer) for each data point"""
         T = self._GEMM(X)
-        return torch.argmax(T,dim=1)
+        return np.argmax(T, axis=1)
     
     def predict_onehot(self, X):
         """One Hot Encoding version of self.predict"""
@@ -94,8 +79,8 @@ class RandomForestGEMM:
     
     def vote(self, X):
         """Count the vote from each tree for each data point"""
-        return torch.stack([e.predict_onehot(X) for e in self.trees], dim=0).sum(dim=0)
+        return np.sum([e.predict_onehot(X) for e in self.trees], axis=0)
 
     def predict(self, X):
         predictions = self.vote(X)
-        return torch.argmax(predictions, dim=1)
+        return np.argmax(predictions, axis=1)
